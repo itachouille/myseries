6e8081977508ac3e45d09892b8b705582af10637
itachouille <anthony.david34@sfr.fr>
2023-08-17 14:20:28 +0200

    '#'

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-		proxy: {
-			'/api': {
-				target: 'https://myseries.onrender.com',
-			},
-		},
-	},
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
 })
