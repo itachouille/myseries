c697356dc3fbf04e120723992119a4b04342c003
itachouille <anthony.david34@sfr.fr>
2023-08-17 15:25:35 +0200

    modif de cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 8500;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://my-series.netlify.app',
+  methods: ['GET', 'POST','PUT' ,'DELETE'],
+  credentials: true,
+}));
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser());
