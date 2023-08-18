854cdfc542018376796cdb61708380395a723ce6
itachouille <anthony.david34@sfr.fr>
2023-08-18 14:56:20 +0200

    '#'

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -41,7 +41,7 @@ export const login = async (req, res, next) => {
 		return res
 		.cookie('access_token', token, {
 			httpOnly: true,
-			//sameSite: false,
+			sameSite: "none",
 		})
 			.status(200)
 			.json({ name: user.name, email: user.email, message: 'login success' });
