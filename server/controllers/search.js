567a61ca7cfb687324db0d674836a489eef383eb
itachouille <anthony.david34@sfr.fr>
2023-11-11 10:59:43 +0100

    search query verification

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,10 +1,14 @@
-// server.js
 import "dotenv/config";
 import fetch from "node-fetch";
 
 export const search = async (req, res) => {
   try {
     const { query } = req.body;
+
+    if (typeof query !== 'string') {
+      return res.status(400).json({ error: "'search' must be a string" });
+    }
+
     const apiKey = process.env.TMDB_API_KEY;
 
     const url = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${query}`;
@@ -12,7 +16,7 @@ export const search = async (req, res) => {
     const data = await response.json();
     res.json(data);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Erreur lors de la recherche" });
+    console.error(error);
+    res.status(500).json({ error: "Error during search" });
   }
 };
