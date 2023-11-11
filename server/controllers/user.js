dbd8e533b003f9363cc2bc3828db51a6577f6354
itachouille <anthony.david34@sfr.fr>
2023-11-11 10:48:31 +0100

    user logic

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -20,7 +20,7 @@ export const updateUser = async (req, res, next) => {
 
 export const getUserInfo = async (req, res, next) => {
 	try {
-		const data = await User.findById(req.user.id).select('name email');
+		const data = await User.findById(req.user.id).select('+password');
 		return res.status(200).json(data);
 	} catch (err) {
 		return next(err);
